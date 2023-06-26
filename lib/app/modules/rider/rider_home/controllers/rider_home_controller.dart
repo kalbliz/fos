12266c850b5bb3bd786ac09c5b1047020362f064
Kareem Victor <44582204+kalbliz@users.noreply.github.com>
2023-06-26 09:54:48 +0100

    UPDATE

--- lib/app/modules/rider/rider_home/controllers/rider_home_controller.dart
+++ lib/app/modules/rider/rider_home/controllers/rider_home_controller.dart
@@ -2,18 +2,15 @@ import 'dart:async';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
 import 'package:fos/app/utilities/enums/view_state.dart';
 import 'package:fos/app/utilities/helpers/d.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class RiderHomeController extends GetxController {
   //TODO: Implement RiderHomeController
-  late Uint8List markerIcon;
-  final GlobalKey fabKey = GlobalKey();
-  final count = 0.obs;
-  final isShimmerLoading = false.obs;
+    final isShimmerLoading = false.obs;
   final visibility = true.obs;
   final switchStatus = false.obs;
   final switch1Status = false.obs;
@@ -24,16 +21,108 @@ class RiderHomeController extends GetxController {
   final deliveryRequestsViewState = ViewState.idle.obs;
   final getDirectionViewState = ViewState.idle.obs;
   final requestsViewState = ViewState.idle.obs;
+ late Uint8List markerIcon;
+RxSet<Marker> markers = <Marker>{}.obs;
+  final Completer<GoogleMapController> mapCompleter =
+      Completer<GoogleMapController>();
 
-  late MapController mapController;
+  late GoogleMapController mapController;
+  late StreamSubscription<Position> positionStream;
 
+  CameraPosition cameraPosition = const CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  Future<Position?> _determinePosition() async {
+    bool? serviceEnabled;
+    Position? position;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      // return null;
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return null;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      Geolocator.openAppSettings();
+      return null;
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    try {
+      position = await Geolocator.getCurrentPosition();
+    } catch (e) {
+      position = null;
+    }
+    return position;
+    // Test if location services are enabled.
+    permission = await Geolocator.checkPermission();
+  }
+
+  void onMapCreated(GoogleMapController controller) async {
+    mapCompleter.complete(controller);
+    mapController = await mapCompleter.future;
+    Position? position = await _determinePosition();
+    if (position != null) {
+      mapController.animateCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(
+            target: LatLng(position.latitude, position.longitude),
+            zoom: 15.7,
+          ),
+        ),
+      );
+      markerIcon =
+          await getBytesFromAsset('assets/images/home/map_marker.png', 110);
+      final Marker riderMarker = Marker(
+        anchor: const Offset(0.5, 1.0),
+        zIndex: 4.0,
+        markerId: const MarkerId(''),
+        icon: BitmapDescriptor.fromBytes(markerIcon),
+        position: LatLng(position.latitude, position.longitude),
+        // infoWindow: const InfoWindow(title: 'fullname'),
+      );
+      markers.clear();
+      markers.add(riderMarker);
+    }
+    // mapController = controller;
+    // if (GeoLocatorService.position != null) {
+    //   await setDriverMarker();
+    //   update();
+    // }
+    // position = LatLng(GeoLocatorService.position?.latitude ?? 0.0,
+    //     GeoLocatorService.position?.longitude ?? 0.0);
+    // Future.delayed(Duration(seconds: 1), () async {
+    //   GoogleMapController controller = await mapCompleter.future;
+    //   controller.animateCamera(
+    //     CameraUpdate.newCameraPosition(
+    //       CameraPosition(
+    //         target: position,
+    //         zoom: 14.0,
+    //       ),
+    //     ),
+    //   );
+    // });
+  }
+  final count = 0.obs;
   @override
   void onInit() {
     super.onInit();
-    mapController = MapController(
-      initMapWithUserPosition: true,
-     
-    );
   }
 
   @override
--- lib/app/modules/rider/rider_home/views/rider_home_view.dart
+++ lib/app/modules/rider/rider_home/views/rider_home_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
 import 'package:flutter_switch/flutter_switch.dart';
 import 'package:fos/app/routes/app_pages.dart';
 import 'package:fos/app/utilities/colors/app_colors.dart';
@@ -9,6 +8,7 @@ import 'package:fos/app/utilities/loader/progress.dart';
 import 'package:fos/app/utilities/responsive/size_fit.dart';
 
 import 'package:get/get.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import '../controllers/rider_home_controller.dart';
 
@@ -19,158 +19,125 @@ class RiderHomeView extends GetView<RiderHomeController> {
     return Scaffold(
       body: SafeArea(
         child: SizedBox(
-          child: Stack(
-            children: [
-              OSMFlutter(
-                isPicker: true,
-                showZoomController: true,
-                controller: controller.mapController,
-                trackMyPosition: true,
-                initZoom: 2,
-                minZoomLevel: 8,
-                maxZoomLevel: 14,
-                stepZoom: 1.0,
-                userLocationMarker: UserLocationMaker(
-                  personMarker: const MarkerIcon(
-                    icon: Icon(
-                      Icons.location_history_rounded,
-                      color: Colors.red,
-                      size: 48,
-                    ),
-                  ),
-                  directionArrowMarker: const MarkerIcon(
-                    icon: Icon(
-                      Icons.double_arrow,
-                      size: 48,
-                    ),
-                  ),
+          child: Obx(() {
+            return Stack(
+              children: [
+                GoogleMap(
+                  initialCameraPosition: controller.cameraPosition,
+                  mapToolbarEnabled: false,
+                  onMapCreated: controller.onMapCreated,
+                  zoomControlsEnabled: false,
+                  // mapType: MapType.terrain,
+                  markers: controller.markers.value,
                 ),
-                roadConfiguration: RoadConfiguration(
-                  startIcon: const MarkerIcon(
-                    icon: Icon(
-                      Icons.person,
-                      size: 64,
-                      color: Colors.brown,
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: Container(
+                    padding: EdgeInsets.only(
+                      top: sizeFit(false, 20, context),
+                      left: sizeFit(true, 16, context),
                     ),
-                  ),
-                  roadColor: Colors.yellowAccent,
-                ),
-                markerOption: MarkerOption(
-                    defaultMarker: const MarkerIcon(
-                  icon: Icon(
-                    Icons.person_pin_circle,
-                    color: Colors.blue,
-                    size: 56,
-                  ),
-                )),
-              ),
-              Align(
-                alignment: Alignment.topCenter,
-                child: Container(
-                  padding: EdgeInsets.only(
-                    top: sizeFit(false, 20, context),
-                    left: sizeFit(true, 16, context),
-                  ),
-                  height: sizeFit(false, 65, context),
-                  decoration: BoxDecoration(
-                    color: AppColors.AppBackgroundWhite.withOpacity(0.5),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Obx(() => controller.toggleViewState.value ==
-                              ViewState.busy
-                          ? Center(
-                              child: Row(
-                                children: [
-                                  const CircularProgressIndicator(
-                                    backgroundColor:
-                                        AppColors.AppCircularProgressLightBlue,
-                                    valueColor: AlwaysStoppedAnimation<Color>(
-                                      AppColors.AppLightBlue,
+                    height: sizeFit(false, 65, context),
+                    decoration: BoxDecoration(
+                      color: AppColors.AppBackgroundWhite.withOpacity(0.5),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Obx(() => controller.toggleViewState.value ==
+                                ViewState.busy
+                            ? Center(
+                                child: Row(
+                                  children: [
+                                    const CircularProgressIndicator(
+                                      backgroundColor: AppColors
+                                          .AppCircularProgressLightBlue,
+                                      valueColor: AlwaysStoppedAnimation<Color>(
+                                        AppColors.AppLightBlue,
+                                      ),
+                                      strokeWidth: 2,
                                     ),
-                                    strokeWidth: 2,
-                                  ),
-                                  SizedBox(
-                                    width: sizeFit(true, 12, context),
-                                  ),
-                                  Text(controller.switchStatus.value
-                                      ? 'Going offline...'
-                                      : 'Going online...'),
-                                ],
-                              ),
-                            )
-                          : FlutterSwitch(
-                              onToggle: (bool value) async {},
-                              width: sizeFit(true, 42, context),
-                              height: sizeFit(false, 26, context),
-                              valueFontSize: sizeFit(false, 16, context),
-                              toggleSize: sizeFit(false, 24, context),
-                              value: controller.switchStatus.value,
-                              borderRadius: sizeFit(false, 22, context),
-                              activeColor: AppColors.AppSwitchGreen,
-                              padding: 0.7,
-                              showOnOff: false,
-                            )),
-                    ],
+                                    SizedBox(
+                                      width: sizeFit(true, 12, context),
+                                    ),
+                                    Text(controller.switchStatus.value
+                                        ? 'Going offline...'
+                                        : 'Going online...'),
+                                  ],
+                                ),
+                              )
+                            : FlutterSwitch(
+                                onToggle: (bool value) async {},
+                                width: sizeFit(true, 42, context),
+                                height: sizeFit(false, 26, context),
+                                valueFontSize: sizeFit(false, 16, context),
+                                toggleSize: sizeFit(false, 24, context),
+                                value: controller.switchStatus.value,
+                                borderRadius: sizeFit(false, 22, context),
+                                activeColor: AppColors.AppSwitchGreen,
+                                padding: 0.7,
+                                showOnOff: false,
+                              )),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              // Align(
-              //   alignment: Alignment.bottomCenter,
-              //   child: Get.find<DeliveryService>().isRiderInTransit.value
-              //       ? Column(
-              //           mainAxisSize: MainAxisSize.min,
-              //           children: [
-              //             ConfirmDeliveryButton(
-              //               onTap: () async {
-              //                 await controller.updateDeliveryStatus(
-              //                     'DELIVERED', false);
-              //               },
-              //             ),
-              //             SizedBox(
-              //               height: sizeFit(false, 32, context),
-              //             ),
-              //           ],
-              //         )
-              //       : SizedBox(
-              //           height: sizeFit(false, 235, context),
-              //           child: controller.deliveryRequestsViewState.value ==
-              //                   ViewState.busy
-              //               ? const ProgressWidget()
-              //               : Offstage(
-              //                   offstage: controller.toggleViewState.value ==
-              //                       ViewState.busy,
-              //                   child: Padding(
-              //                     padding: EdgeInsets.symmetric(
-              //                       horizontal: sizeFit(true, 8, context),
-              //                       vertical: 8,
-              //                     ),
-              //                     child: controller.requests.data!.isEmpty
-              //                         ? SizedBox()
-              //                         : ListView.separated(
-              //                             shrinkWrap: true,
-              //                             physics:
-              //                                 const ClampingScrollPhysics(),
-              //                             scrollDirection: Axis.horizontal,
-              //                             itemCount: controller
-              //                                 .requests.data!.length,
-              //                             separatorBuilder: (context, index) {
-              //                               return SizedBox(
-              //                                 width:
-              //                                     sizeFit(true, 8, context),
-              //                               );
-              //                             },
-              //                             itemBuilder: (context, index) {
-              //                               return IncomingDeliveryRequest(
-              //                                   index: index);
-              //                             }),
-              //                   ),
-              //                 ),
-              //         ),
-              // ),
-            ],
-          ),
+                // Align(
+                //   alignment: Alignment.bottomCenter,
+                //   child: Get.find<DeliveryService>().isRiderInTransit.value
+                //       ? Column(
+                //           mainAxisSize: MainAxisSize.min,
+                //           children: [
+                //             ConfirmDeliveryButton(
+                //               onTap: () async {
+                //                 await controller.updateDeliveryStatus(
+                //                     'DELIVERED', false);
+                //               },
+                //             ),
+                //             SizedBox(
+                //               height: sizeFit(false, 32, context),
+                //             ),
+                //           ],
+                //         )
+                //       : SizedBox(
+                //           height: sizeFit(false, 235, context),
+                //           child: controller.deliveryRequestsViewState.value ==
+                //                   ViewState.busy
+                //               ? const ProgressWidget()
+                //               : Offstage(
+                //                   offstage: controller.toggleViewState.value ==
+                //                       ViewState.busy,
+                //                   child: Padding(
+                //                     padding: EdgeInsets.symmetric(
+                //                       horizontal: sizeFit(true, 8, context),
+                //                       vertical: 8,
+                //                     ),
+                //                     child: controller.requests.data!.isEmpty
+                //                         ? SizedBox()
+                //                         : ListView.separated(
+                //                             shrinkWrap: true,
+                //                             physics:
+                //                                 const ClampingScrollPhysics(),
+                //                             scrollDirection: Axis.horizontal,
+                //                             itemCount: controller
+                //                                 .requests.data!.length,
+                //                             separatorBuilder: (context, index) {
+                //                               return SizedBox(
+                //                                 width:
+                //                                     sizeFit(true, 8, context),
+                //                               );
+                //                             },
+                //                             itemBuilder: (context, index) {
+                //                               return IncomingDeliveryRequest(
+                //                                   index: index);
+                //                             }),
+                //                   ),
+                //                 ),
+                //         ),
+                // ),
+              ],
+            );
+          }),
         ),
       ),
     );
